953f686821714820ed832f9984d4aed6d2741189
Yasmin Hester <yasminyasale9@gmail.com>
2026-05-27 22:04:17 -0400

    rotas de culturas criadas (ainda nao testadas)

--- sigfaz/backend/controllers/culturas.py
+++ sigfaz/backend/controllers/culturas.py
@@ -1,32 +1,99 @@
 from fastapi import APIRouter
+from models.model import Cultura
+from db import conectar
 
-router_culturas = APIRouter(prefix="/culturas")
+router_culturas = APIRouter(
+    prefix="/culturas",
+    tags=["Cultura"]
+)
 
-@router_culturas.get('/')  
-
-def consultar_culturas():
-    pass
-
-
-@router_culturas.get('/{id}')  
-
-def consultar_cultura(id: int):
-    pass
-
-
+# cadastrar
 @router_culturas.post('/')  
+def cadastrar_cultura(cultura: Cultura):
+    conn = conectar()
+    cursor = conn.cursor
 
-def cadastrar_culturas():
-    pass
+    sql = """
+    INSERT INTO Cultura
+    (nome, safra)
+    VALUES (%s, %s)
+    """
 
+    valores = (
+        cultura.nome,
+        cultura.safra
+    )
 
+    cursor.execute(sql, valores)
+
+    conn.commit()
+
+    cursor.close()
+    conn.close()
+
+    return {"mensagem": "Cultura cadastrada"}
+    
+
+# listar
+@router_culturas.get('/')  
+def listar_culturas(id: int):
+    conn = conectar()
+    cursor = conn.cursor()
+
+    cursor.execute("SELECT * FROM Cultura")
+
+    dados = cursor.fetchall()
+
+    cursor.close()
+    conn.close()
+
+    return dados
+
+# alterar
 @router_culturas.put('/{id}')  
 
-def alterar_cultura(id: int):
-    pass
+def alterar_cultura(id: int, cultura: Cultura):
+    
+    conn = conectar()
+    cursor = conn.cursor()
 
+    sql = """
+    UPDATE Cultura
+    SET nome=%s,
+        safra=%s
+    WHERE id=%s
+    """
 
+    valores = (
+        cultura.nome,
+        cultura.safra,
+        id
+    )
+
+    cursor.execute(sql, valores)
+
+    conn.commit()
+
+    cursor.close()
+    conn.close()
+
+    return {"mensagem": "Cultura atualizada"}
+
+# deletar
 @router_culturas.delete('/{id}')  
-
 def deletar_cultura(id: int):
-    pass
+    
+    conn = conectar()
+    cursor = conn.cursor()
+
+    cursor.execute(
+        "DELETE FROM Cultura WHERE id=%s",
+        (id,)
+    )
+
+    conn.commit()
+
+    cursor.close()
+    conn.close()
+
+    return {"mensagem": "Cultura deletada"}
